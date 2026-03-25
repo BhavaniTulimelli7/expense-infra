595bf381c343908d7d22a183dae119c06705a058
Tulimelli Rajani <tulimellirajani@gmail.com>
2026-03-25 11:35:53 +0530

    committing infra files

---
+++ 60-acm/variables.tf
@@ -0,0 +1,23 @@
+variable "project_name" {
+    default = "expense"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "common_tags" {
+    default = {
+        Project = "expense"
+        Environment = "dev"
+        Terraform = "true"
+    }
+}
+
+variable "domain_name" {
+    default = "krishiv.store"
+}
+
+variable "zone_id" {
+    default = "Z06482001IVMWCKDNIN81"
+}
