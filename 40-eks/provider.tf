16b680dba43888a141375fc5d573c80e9bbb9a78
Tulimelli Rajani <tulimellirajani@gmail.com>
2026-03-25 15:20:07 +0530

    committing infra files

--- 40-eks/provider.tf
+++ 40-eks/provider.tf
@@ -2,9 +2,18 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "6.12.0"
+      version = "~> 5.95"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = ">= 3.0.0"
+    }
+    time = {
+      source  = "hashicorp/time"
+      version = ">= 0.9.0"
     }
   }
+
 
   backend "s3" {
     bucket = "expense-dev-bhavani"
