79628b04b5c25f1ffff65c518d9d52918009205c
Tulimelli Rajani <tulimellirajani@gmail.com>
2026-03-25 15:45:03 +0530

    committing infra files

--- 40-eks/main.tf
+++ 40-eks/main.tf
@@ -9,8 +9,8 @@ module "eks" {
 
   cluster_name    = local.name
   cluster_version = "1.32" # later we upgrade 1.32
-  create_node_security_group = false
-  create_cluster_security_group = false
+  create_node_security_group = true
+  create_cluster_security_group = true
   cluster_security_group_id = local.eks_control_plane_sg_id
   node_security_group_id = local.eks_node_sg_id
 
